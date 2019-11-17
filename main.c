577746108ec251211240fa69db1ab809324901c4
Styg <matthieu.hirth.68@gmail.com>
2019-11-17 13:38:32 +0100

    Création fonction générateur de processus

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
+
+typedef struct {
+    long priorite;
+    int pid;
+    int dateSoumission;
+    int tpsExec;
+} processus;
 
 int main() {
-    printf("Hello, World!\n");
+    ProcessusGenerateur();
     return 0;
+}
+
+void ProcessusGenerateur() {
+
+    if(fork() == 0) {
+        
+        processus process;
+        process.pid = getpid();
+        process.priorite = rand()%11;
+        process.tpsExec = rand()%21;
+        process.dateSoumission = rand()%6;
+        printf("[Processus lancé] PID : %ld  priorite", process.priorite);
+
+        while(1) {
+
+        }
+    }
 }
